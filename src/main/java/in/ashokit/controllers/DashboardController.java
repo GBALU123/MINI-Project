db7017fcfee21c442204c66ab187561e5837ec77
balu <gangulabalachandrudu123@gmail.com>
2024-02-06 11:49:12 +0530

    first commit

---
+++ src/main/java/in/ashokit/controllers/DashboardController.java
@@ -0,0 +1,25 @@
+package in.ashokit.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.ashokit.services.DashboardService;
+
+@Controller
+public class DashboardController {
+	@Autowired
+	private DashboardService dashboardService;
+
+	@GetMapping("/dashboard")
+	public String buildDashboard(Model model) {
+
+		String quoteTxt = dashboardService.getQuote();
+
+		model.addAttribute("quote", quoteTxt);
+
+		return "dashboard";
+	}
+
+}
